3ac6e7fe15c8a512a061c2dc482693d19f18d6ee
mae meier <maemeier@live.com>
2019-10-12 21:37:17 +0200

    create table

--- src/App.js
+++ src/App.js
@@ -1,52 +1,73 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './style.css';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./style.css";
 
 // create data
-const products =[
-  {id:1, name:"iphone", color:"Stonegrey", price:789},
-  {id:2, name:"Samsung", color:"Midnight Blck", price:569},
-  {id:3, name:"Hauwai P9", color:"Mystic Silver", price:272}
+const products = [
+  { id: 1, name: "iphone", color: "Stonegrey", price: 789 },
+  { id: 2, name: "Samsung", color: "Midnight Blck", price: 569 },
+  { id: 3, name: "Hauwai P9", color: "Mystic Silver", price: 272 }
 ];
 
 // get data
 
-localStorage.setItem("products", JSON.stringify(products))
+localStorage.setItem("products", JSON.stringify(products));
 
-class App extends Component{
+class App extends Component {
+  componentWillMount() {
+    const products = this.getProducts();
+  }
 
+  getProducts() {
+    const products = JSON.parse(localStorage.getItem("products"));
+    console.log(products);
 
-componentWillMount(){
- const products = this.getProducts();
-}
+    this.setState({ products });
+  }
 
-getProducts(){
-  const products = JSON.parse(localStorage.getItem("products"))
-  console.log(products);
+  handleAddProduct() {}
 
-  this.setState({products})
-}
+  handleUpdateProduct() {}
 
-handleAddProduct(){
+  handleDeleteProduct() {}
 
-}
-
-handleUpdateProduct(){
-
-}
-
-handleDeleteProduct(){
-
-}
-
-
-render (){
-  return(
-    <div>
-He
-    </div>
-  )
-}
+  render() {
+    return (
+      <div>
+        <h2> Smart phones stock</h2>
+        <div className="container">
+          <div className="inputBox">
+            <p>Input your data</p>
+            <form>
+              <div className="form">
+                <strong>Name:</strong>
+                <input type="text" placeHolder="Product Name" refs="name" />
+              </div>
+              <div className="form">
+                <strong>Color:</strong>
+                <input type="text" placeHolder="Color" refs="color" />
+              </div>
+              <div className="form">
+                <strong>Price:</strong>
+                <input type="number" placeHolder=" Price" refs="price" />
+              </div>
+              <button className="submitButton">Submit</button>
+            </form>
+          </div>
+          <div className="displayBox">
+            <table>
+              <tr>
+                <th className="headerName">Product</th>
+                <th className="headerName">Color</th>
+                <th className="headerName">Price</th>
+                <th className="headerName">Edit</th>
+                <th className="headerName">Delete</th>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
