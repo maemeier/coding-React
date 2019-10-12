a77003706f639fccd2049a8db735853672aca6a7
mae meier <maemeier@live.com>
2019-10-12 21:44:56 +0200

    update addproduct component

---
+++ src/components/addProduct.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class addProduct extends Component {
+  render() {
+    return <div>This is Add Product Component</div>;
+  }
+}
+
+export default addProduct;
